a93d4f5b499c32ced00fb09dbbecc63b03a944a2
gaochangjian <gaochangjian@2906bd92-c761-11dd-aa6a-075d27645067>
2008-12-23 05:46:32 +0000

    Added some comments.

--- maze.c
+++ maze.c
@@ -17,6 +17,9 @@ typedef struct
 int step;
 int stepset[100];
 
+/*
+ * Random create the crazy maze.
+ */
 void CreateMaze(SElemType (*maze)[COLUMN], int row, int col)
 {
   int i, j;
@@ -24,66 +27,79 @@ void CreateMaze(SElemType (*maze)[COLUMN], int row, int col)
   for (i = 0; i != row; ++i)
     for (j = 0; j != col; ++j)
     {
-      if (i == 0 || j == 0 || i == row - 1 || j == col - 1)
+      /*
+       * The meaning of variable 'grid':
+       *   grid=0: this point is a pathway
+       *   grid=1: this point is a wall
+       */
+      if (i == 0 || j == 0 || i == row - 1 || j == col - 1) // build the wall
         maze[i][j].grid = 1;
-      else if ((i == 1 && j == 1) || (i == row - 2 && j == col - 2))
+      else if ((i == 1 && j == 1) || (i == row - 2 && j == col - 2)) // build the entry and exit
       {
-        maze[i][j].grid = 0;
-        maze[i][j].direction = 0;
-        maze[i][j].pass = 0;
+        maze[i][j].grid = 0;    // this is a pathway
+        maze[i][j].direction = 0; // initial direction is east
+        maze[i][j].pass = 0;      // haven't passed
         maze[i][j].i = i;
         maze[i][j].j = j;
       }
       else
       {
         /*
-         * random create
+         * Random build every grid.
+         * If the random number is less than 5, then this grid is a pathway,
+         * else it is a wall.
          */
         if ((rand() % 10) < 5)
         {
-          maze[i][j].grid = 0;
-          maze[i][j].direction = 0;
-          maze[i][j].pass = 0;
+          maze[i][j].grid = 0;  // this is a pathway
+          maze[i][j].direction = 0; // initial direction is east
+          maze[i][j].pass = 0;      // haven't passed
           maze[i][j].i = i;
           maze[i][j].j = j;
         }
         else
-          maze[i][j].grid = 1;
+          maze[i][j].grid = 1;  // this is a wall
       }
     }
 }
 
+/*
+ * Print the maze.
+ */
 void PrintMaze(SElemType (*maze)[COLUMN], int row, int col)
 {
   int i, j;
 
+  /*
+   * This loop judge every grid, it can be a wall, a pathway, or a gateway.
+   */
   for (i = 0; i != row; ++i)
   {
     for (j = 0; j != col; ++j)
     {
-      if (maze[i][j].pass == 2)
+      if (maze[i][j].pass == 2) // pass equle to 2 means this grid is a gateway
       {
         switch (maze[i][j].direction)
         {
-          case 1 :
+          case 1 :              // east
             printf("→ ");
             break;
-          case 2 :
+          case 2 :              // south
             printf("↓ ");
             break;
-          case 3 :
+          case 3 :              // west
             printf("← ");
             break;
-          case 4 :
+          case 4 :              // north
             printf("↑ ");
             break;
-          default :
+          default :             // exit
             printf("￥");
         }
       }
-      else if (maze[i][j].grid)
+      else if (maze[i][j].grid) // this grid is a wall
         printf("▇ ");
-      else
+      else                      // this grid is a pathway
         printf("  ");
     }
 
@@ -220,39 +236,47 @@ int main()
 {
   srand(time(NULL));
 
+  /*
+   * Random get the row number, but the column can't.
+   */
   int row = rand() % 47 + 4, col = COLUMN;
   // SElemType* maze = (SElemType*) malloc(row * sizeof(col * sizeof(SElemType)));
-  SElemType maze[row][col];
+  SElemType maze[row][col];  // store the maze map
 
-  SqStack path, pathway;
-  SElemType e;
+  SqStack path, pathway; // 'path' is the way through the maze from exit to entry, 'pathway' reverse it.
+  SElemType e;           // temporary variable
   InitStack(&path);
   InitStack(&pathway);
 
+  /*
+   * Random create the maze until it has a right pathway.
+   */
   do
   {
     CreateMaze(maze, row, col);
   }
   while (!Maze(maze, &path, row, col));
 
-  if (!StackEmpty(path))
+  /*
+   * Pop from 'path', then push 'pathway', get the pathway from entry to exit.
+   */
+  while (!StackEmpty(path))
   {
-    while (!StackEmpty(path))
-    {
-      Pop(&path, &e);
-      Push(&pathway, e);
-    }
-    while (!StackEmpty(pathway))
-    {
-      Pop(&pathway, &e);
-      maze[e.i][e.j].pass = 2;
-      system("clear");
-      PrintMaze(maze, row, col);
-      system("sleep 0.5s");
-    }
+    Pop(&path, &e);
+    Push(&pathway, e);
   }
-  else
-    printf("no way\n");
+
+  /*
+   * Print the right pathway dynamically.
+   */
+  while (!StackEmpty(pathway))
+  {
+    Pop(&pathway, &e);
+    maze[e.i][e.j].pass = 2;    // tag this point is the right pathway
+    system("clear");            // refresh
+    PrintMaze(maze, row, col);
+    system("sleep 0.5s");       // refresh time
+  }
 
   return 0;
 }
